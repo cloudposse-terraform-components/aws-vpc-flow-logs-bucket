7ebbe07c818f4c685d8cac3e178698e94f26c226
Igor Rodionov <goruha@gmail.com>
2024-11-02 19:22:15 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,9 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "vpc_flow_logs_bucket_id" {
+  value       = module.flow_logs_s3_bucket.bucket_id
+  description = "VPC Flow Logs bucket ID"
+}
+
+output "vpc_flow_logs_bucket_arn" {
+  value       = module.flow_logs_s3_bucket.bucket_arn
+  description = "VPC Flow Logs bucket ARN"
 }
