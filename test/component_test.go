1ce2b2db83beee36e6b07ddf79f0feac07545d78
Igor Rodionov <goruha@gmail.com>
2025-02-17 18:30:17 +0100

    Refactor tests

--- test/component_test.go
+++ test/component_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 
 	"github.com/cloudposse/test-helpers/pkg/atmos"
-	helper "github.com/cloudposse/test-helpers/pkg/atmos/aws-component-helper"
+	helper "github.com/cloudposse/test-helpers/pkg/atmos/component-helper"
 	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/stretchr/testify/assert"
 )
@@ -21,74 +21,82 @@ type BucketPolicy struct {
 		} `json:"Principal,omitempty"`
 		Effect    string      `json:"Effect"`
 		Action    string      `json:"Action"`
-		Resource  interface{} `json:"Resource"` // Changed to interface{} to accommodate array
+		Resource  interface{} `json:"Resource"`
 		Condition struct {
 			StringEquals    map[string]string   `json:"StringEquals,omitempty"`
 			StringNotEquals map[string][]string `json:"StringNotEquals,omitempty"`
 			Null            map[string]string   `json:"Null,omitempty"`
-			Bool            map[string]bool     `json:"Bool,omitempty"` // Added Bool for new condition
+			Bool            map[string]bool     `json:"Bool,omitempty"`
 			ArnLike         map[string][]string `json:"ArnLike,omitempty"`
 		} `json:"Condition"`
 	} `json:"Statement"`
 }
 
-func TestComponent(t *testing.T) {
-	awsRegion := "us-east-2"
+type ComponentSuite struct {
+	helper.TestSuite
+}
 
-	fixture := helper.NewFixture(t, "../", awsRegion, "test/fixtures")
+func (s *ComponentSuite) TestBasic() {
+	const component = "vpc-flow-logs-bucket/basic"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
 
-	defer fixture.TearDown()
-	fixture.SetUp(&atmos.Options{})
+	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
+	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
+	assert.NotNil(s.T(), options)
 
-	fixture.Suite("default", func(t *testing.T, suite *helper.Suite) {
-		suite.Test(t, "basic", func(t *testing.T, atm *helper.Atmos) {
-			defer atm.GetAndDestroy("vpc-flow-logs-bucket/basic", "default-test", map[string]interface{}{})
-			component := atm.GetAndDeploy("vpc-flow-logs-bucket/basic", "default-test", map[string]interface{}{})
-			assert.NotNil(t, component)
+	bucketID := atmos.Output(s.T(), options, "vpc_flow_logs_bucket_id")
+	assert.NotEmpty(s.T(), bucketID)
 
-			bucketID := atm.Output(component, "vpc_flow_logs_bucket_id")
-			assert.NotEmpty(t, bucketID)
+	bucketArn := atmos.Output(s.T(), options, "vpc_flow_logs_bucket_arn")
+	assert.True(s.T(), strings.HasSuffix(bucketArn, bucketID))
 
-			bucketArn := atm.Output(component, "vpc_flow_logs_bucket_arn")
-			assert.True(t, strings.HasSuffix(bucketArn, bucketID))
+	actualStatus := aws.GetS3BucketVersioning(s.T(), awsRegion, bucketID)
+	expectedStatus := "Suspended"
+	assert.Equal(s.T(), expectedStatus, actualStatus)
 
-			// Verify that our Bucket has versioning enabled
-			actualStatus := aws.GetS3BucketVersioning(t, awsRegion, bucketID)
-			expectedStatus := "Suspended"
-			assert.Equal(t, expectedStatus, actualStatus)
+	policyString := aws.GetS3BucketPolicy(s.T(), awsRegion, bucketID)
 
-			policyString := aws.GetS3BucketPolicy(t, awsRegion, bucketID)
+	var policy BucketPolicy
+	json.Unmarshal([]byte(policyString), &policy)
 
-			var policy BucketPolicy
-			json.Unmarshal([]byte(policyString), &policy)
+	statement := policy.Statement[0]
+	assert.Equal(s.T(), "ForceSSLOnlyAccess", statement.Sid)
+	assert.Equal(s.T(), "s3:*", statement.Action)
+	assert.Equal(s.T(), "Deny", statement.Effect)
+	assert.ElementsMatch(s.T(), []string{
+		fmt.Sprintf("arn:aws:s3:::%s/*", bucketID),
+		fmt.Sprintf("arn:aws:s3:::%s", bucketID),
+	}, statement.Resource)
+	assert.Equal(s.T(), false, statement.Condition.Bool["aws:SecureTransport"])
 
-			statement := policy.Statement[0]
+	statement = policy.Statement[1]
+	assert.Equal(s.T(), "AWSLogDeliveryWrite", statement.Sid)
+	assert.Equal(s.T(), "Allow", statement.Effect)
+	assert.Equal(s.T(), "delivery.logs.amazonaws.com", statement.Principal.Service)
+	assert.Equal(s.T(), "s3:PutObject", statement.Action)
+	assert.Equal(s.T(), fmt.Sprintf("arn:aws:s3:::%s/*", bucketID), statement.Resource)
+	assert.Equal(s.T(), "bucket-owner-full-control", statement.Condition.StringEquals["s3:x-amz-acl"])
 
-			assert.Equal(t, "ForceSSLOnlyAccess", statement.Sid)
-			assert.Equal(t, "s3:*", statement.Action)
-			assert.Equal(t, "Deny", statement.Effect)
-			assert.ElementsMatch(t, []string{
-				fmt.Sprintf("arn:aws:s3:::%s/*", bucketID),
-				fmt.Sprintf("arn:aws:s3:::%s", bucketID),
-			}, statement.Resource) // Check for multiple resources
-			assert.Equal(t, false, statement.Condition.Bool["aws:SecureTransport"]) // Check the Bool condition
+	statement = policy.Statement[2]
+	assert.Equal(s.T(), "AWSLogDeliveryAclCheck", statement.Sid)
+	assert.Equal(s.T(), "Allow", statement.Effect)
+	assert.Equal(s.T(), "delivery.logs.amazonaws.com", statement.Principal.Service)
+	assert.Equal(s.T(), "s3:GetBucketAcl", statement.Action)
+	assert.Equal(s.T(), fmt.Sprintf("arn:aws:s3:::%s", bucketID), statement.Resource)
 
-			statement = policy.Statement[1]
+	s.DriftTest(component, stack, nil)
+}
 
-			assert.Equal(t, "AWSLogDeliveryWrite", statement.Sid)
-			assert.Equal(t, "Allow", statement.Effect)
-			assert.Equal(t, "delivery.logs.amazonaws.com", statement.Principal.Service)
-			assert.Equal(t, "s3:PutObject", statement.Action)
-			assert.Equal(t, fmt.Sprintf("arn:aws:s3:::%s/*", bucketID), statement.Resource)
-			assert.Equal(t, "bucket-owner-full-control", statement.Condition.StringEquals["s3:x-amz-acl"])
+func (s *ComponentSuite) TestEnabledFlag() {
+	const component = "vpc-flow-logs-bucket/disabled"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
 
-			statement = policy.Statement[2]
+	s.VerifyEnabledFlag(component, stack, nil)
+}
 
-			assert.Equal(t, "AWSLogDeliveryAclCheck", statement.Sid)
-			assert.Equal(t, "Allow", statement.Effect)
-			assert.Equal(t, "delivery.logs.amazonaws.com", statement.Principal.Service)
-			assert.Equal(t, "s3:GetBucketAcl", statement.Action)
-			assert.Equal(t, fmt.Sprintf("arn:aws:s3:::%s", bucketID), statement.Resource)
-		})
-	})
+func TestRunSuite(t *testing.T) {
+	suite := new(ComponentSuite)
+	helper.Run(t, suite)
 }
